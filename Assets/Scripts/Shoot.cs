20c7b1e12c484ac7a8d13e2c82b46ee65a57396b
Haven <oilpastel701@gmail.com>
2021-09-13 14:25:43 -0500

    projectile movement

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField] float shootForce = 1000;
+    [SerializeField] ParticleSystem impactParticle;
+    [SerializeField] AudioSource impactClip;
+
+    public void ShootCharge(GameObject bullet)
+    {
+        GameObject shot = Instantiate(bullet, transform.position, transform.rotation);
+        shot.GetComponent<Rigidbody>().AddForce(transform.forward * shootForce);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ParticleSystem particle = Instantiate(impactParticle, this.transform.position, this.transform.rotation);
+        AudioSource audio = Instantiate(impactClip, this.transform.position, this.transform.rotation);
+        Impact();
+        ParticleSystem.Destroy(particle);
+        AudioSource.Destroy(audio);
+        GameObject.Destroy(this);
+    }
+
+    private void Impact()
+    {
+        impactParticle.Play();
+        impactClip.Play();
+
+    }
+
+}
